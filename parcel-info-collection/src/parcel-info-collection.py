0b725208678433a32d557cdf4b86ea6af57f5409
zachbodi <zachbodi@bu.edu>
2021-04-01 23:02:06 -0400

    added scraping for rectangular coordinates; generated file is now uploaded to Google Drive

--- parcel-info-collection/src/parcel-info-collection.py
+++ parcel-info-collection/src/parcel-info-collection.py
@@ -7,9 +7,10 @@ def rads_to_degrees(rads):
 
 def wgs84_to_lat_lon(x,y):
   """
-  The Hartford data uses WGS84 coordinates on a Mercator projection. These formulas come from page 44
-  of the following book: https://pubs.usgs.gov/pp/1395/report.pdf. I calculated R by hand using the
-  longitude/latitude of a known location and the provided WGS84 coordinates for that location.
+  Hartford GIS data uses WGS84 coordinates on a Mercator projection. These formulas come from page 44
+  of the following textbook. I calculated R by hand using the longitude/latitude of a known location
+  and the provided WGS84 coordinates for that location.
+  https://pubs.usgs.gov/pp/1395/report.pdf
   """
 
   R = 6378138.96443
@@ -18,13 +19,22 @@ def wgs84_to_lat_lon(x,y):
 
   return rads_to_degrees(lat), rads_to_degrees(lon)
 
-def get_parcel_spatial_info(address):
+def query_address(address):
+  """
+  Gives possible parcels in Hartford based on input address
+  """
+
   url_endpoint = 'https://gis1.hartford.gov/arcgis/rest/services/AddressPT_Locator_v105/GeocodeServer/findAddressCandidates'
   params = { 'f': 'pjson', 'Single Line Input':address, 'outSR':'{"wkid":102100,"latestWkid":3857}'}
 
   return requests.get(url_endpoint, params=params, timeout=5).json()
 
-def get_parcel_category(location):
+def query_parcel_survey(location):
+  """
+  Gives category and geometry for a parcel in the 2019 All-City Hartford Survey
+  https://gis1.hartford.gov/Html5Viewer/index.html?viewer=AllCitySurvey2019
+  """
+
   url_endpoint = 'https://gis1.hartford.gov/arcgis/rest/services/AllCitySurvey2019Parcels/MapServer/0/query'
   params = {
     'f': 'pjson',
@@ -39,40 +49,72 @@ def get_parcel_category(location):
   }
   
   response = requests.get(url_endpoint, params=params, timeout=5)
-  survey = response.json()
-
-  return survey['features'][0]['attributes']['LovelandSurveyOriginal_category']
+  return response.json()
 
 def get_parcel_data(address):
-  spatial_info = get_parcel_spatial_info(address)
+  parcel_candidates = query_address(address)
 
-  if spatial_info['candidates'] == []:
+  if parcel_candidates['candidates'] == []:
     return None
 
-  location = spatial_info['candidates'][0]['location']
+  # Take first parcel; some addresses give multiple candidates that are actually the same parcel
+  parcel_location = parcel_candidates['candidates'][0]['location']
 
-  return wgs84_to_lat_lon(location['x'], location['y']), get_parcel_category(location)
+  raw_parcel_data = query_parcel_survey(parcel_location)
+
+  if raw_parcel_data['features'] == []:
+    return None
+
+  category = raw_parcel_data['features'][0]['attributes']['LovelandSurveyOriginal_category']
+
+  rings = raw_parcel_data['features'][0]['geometry']['rings'][0]
+
+  min_lat, min_lon, max_lat, max_lon = float('inf'), float('inf'), float('-inf'), float('-inf')
+
+  for coordinates in rings:
+    lat, lon = wgs84_to_lat_lon(coordinates[0], coordinates[1])
+
+    min_lat = lat if lat < min_lat else min_lat
+    max_lat = lat if lat > max_lat else max_lat
+    min_lon = lon if lon < min_lon else min_lon
+    max_lon = lon if lon > max_lon else max_lon
+
+  return {
+    'address': address,
+    'min_latitude': min_lat,
+    'max_latitude': max_lat,
+    'min_longitude': min_lon,
+    'max_longitude': max_lon,
+    'category': category
+  }
 
 def read_parcels_from_csv(filename):
-  addresses = pd.read_csv(filename, usecols=['STREETNUM', 'STREETNAME', 'STREETTYPE'])
+  addresses = pd.read_csv(filename, usecols=['FULL_ADDRESS'])
   addresses = addresses.dropna(axis=0, how='any')
+  addresses = addresses.drop_duplicates(subset=['FULL_ADDRESS'])
 
-  output = pd.DataFrame(columns=['latitude', 'longitude', 'category'])
+  output = pd.DataFrame(columns=
+    [
+    'address',
+    'min_latitude',
+    'max_latitude',
+    'min_longitude',
+    'max_longitude',
+    'category'
+    ]
+  )
 
   for _,row in addresses.iterrows():
-    address = '{} {} {}'.format(row['STREETNUM'], row['STREETNAME'], row['STREETTYPE'])
+    address = row['FULL_ADDRESS']
 
-    try:
-      (lat,lon), category = get_parcel_data(address)
+    parcel_data = get_parcel_data(address)
 
-      new_row = {'latitude':lat, 'longitude':lon, 'category':category}
-      output = output.append(new_row, ignore_index=True)
-
+    if parcel_data:
+      output = output.append(parcel_data, ignore_index=True)
       print('added {}...'.format(address))
-
-    except:
+    else:
       print('failed {}...'.format(address))
 
-  output.to_csv('parcels.csv', index = False)
+  output.to_csv('parcels.csv', index=False)
 
 read_parcels_from_csv('parcel-addresses.csv')
